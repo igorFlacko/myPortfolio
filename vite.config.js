dcd9f62b6b75a4002ed2d077eba22a25f7050a49
CARDOSO DUARTE Igor <igor.cardoso@soprasteria.com>
2024-06-24 18:15:31 +0200

    updated glb model

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// vite.config.js
+export default {
+    server: {
+      open: true,  // Apri il browser automaticamente
+    },
+    assetsInclude: ['**/*.gbl', '**/*.glb'],  // Includi i formati GLB e GLTF
+  }
